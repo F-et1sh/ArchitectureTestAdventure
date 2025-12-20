b0f73e11cb8a4da91f9a4c3fb5389ea28c2e9448
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-12-20 15:31:10 +0300

    resource.hpp done

---
+++ ArchitectureTestAdventure/Code/Core/attributes.hpp
@@ -0,0 +1,22 @@
+/*=================================================
+
+    Copyright (C) 2025 Farrakh.  All Rights Reserved.
+    
+    This source is a part of ArchitectureTestAdventure.
+    Check README.md for more information.
+    
+    File : attributes.hpp
+    
+    Content : useful defines
+
+=================================================*/
+
+#pragma once
+
+#if __cplusplus >= 201703L // C++17
+    #define ATA_MAYBE_UNUSED [[maybe_unused]]
+#else
+    #define ATA_MAYBE_UNUSED
+#endif
+
+#define ATA_NODISCARD [[nodiscard]]
