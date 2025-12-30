eb5a4268d20092fa00c7341fa48d461e30269387
Фаррах Фаттахов <farrakhfattahov@gmail.com>
2025-12-30 15:18:15 +0300

    NVRHI is included

---
+++ ArchitectureTestAdventure/Code/Renderer/Renderer.cpp
@@ -0,0 +1,15 @@
+/*=================================================
+
+    Copyright (C) 2025 Farrakh.  All Rights Reserved.
+    
+    This source is a part of ArchitectureTestAdventure.
+    Check README.md for more information.
+    
+    File : Renderer.cpp
+    
+    Content : Renderer aka RenderSystem of the engine
+
+=================================================*/
+
+#include "pch.hpp"
+#include "Renderer.hpp"
---
+++ ArchitectureTestAdventure/Code/Renderer/Renderer.hpp
@@ -0,0 +1,25 @@
+/*=================================================
+
+    Copyright (C) 2025 Farrakh.  All Rights Reserved.
+    
+    This source is a part of ArchitectureTestAdventure.
+    Check README.md for more information.
+    
+    File : Renderer.hpp
+    
+    Content : Renderer aka RenderSystem of the engine
+
+=================================================*/
+
+#pragma once
+
+namespace ata {
+    class Renderer {
+    public:
+        Renderer() = default;
+        ~Renderer() = default;
+
+    private:
+
+    };
+}
